fe3624743b9e72c37ca1781a5127e18a81a4b3c1
Skywander <c132316@163.com>
2016-03-31 12:36:47 +0800

    Initial Commit

---
+++ 日历/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  日历
+//
+//  Created by 于恩聪 on 16/3/31.
+//  Copyright © 2016年 于恩聪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ 日历/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  日历
+//
+//  Created by 于恩聪 on 16/3/31.
+//  Copyright © 2016年 于恩聪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
